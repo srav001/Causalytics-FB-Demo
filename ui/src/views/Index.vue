5392acafd3492e5176d668e22e9a930ed938543c
srav001 <sravansuresh1x@gmail.com>
2022-09-15 04:59:45 +0530

    feat: implement bar chart component

--- ui/src/views/Index.vue
+++ ui/src/views/Index.vue
@@ -3,17 +3,37 @@ import { onMounted, ref } from 'vue';
 
 import { useAPIstore } from '../stores/apiStore';
 
+import BarChart from '../components/BarChart.vue';
+
 const apiStore = useAPIstore();
 
+const loaded = ref(true);
+
 const FBdata = ref({});
+
+const chartDataSets = ref([]);
 
 onMounted(async () => {
 	FBdata.value = await apiStore.fetchFBdata();
 
-	console.log({ ...FBdata.value });
+	chartDataSets.value = [
+		{
+			label: 'GB CNC Bar Chart',
+			data: FBdata.value.campaignImpresssions
+		}
+	];
+
+	loaded.value = false;
 });
 </script>
 
 <template>
-	<h1>test</h1>
+	<v-card :loading="loaded" class="mx-auto my-12" max-width="800">
+		<template slot="progress">
+			<div class="mx-auto">
+				<v-progress-circular color="deep-purple" height="10" indeterminate></v-progress-circular>
+			</div>
+		</template>
+		<bar-chart v-if="!loaded" :chart-labels="FBdata.campaignNames" :chart-data-sets="chartDataSets"></bar-chart>
+	</v-card>
 </template>
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,21 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+/* eslint-disable import/no-unresolved */
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
-import vue2 from '@vitejs/plugin-vue2'
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import vue2 from '@vitejs/plugin-vue2';
+
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue2(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	plugins: [
+		vue2(),
+		legacy({
+			targets: ['ie >= 11'],
+			additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+		}),
+		Components({
+			resolvers: [VuetifyResolver()]
+		})
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			api: fileURLToPath(new URL('./src/api', import.meta.url)),
+			components: fileURLToPath(new URL('./src/components', import.meta.url)),
+			stores: fileURLToPath(new URL('./src/stores', import.meta.url))
+		}
+	}
+});
