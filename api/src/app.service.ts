0cbaea7a32489f02bc2520e9a207b2424bf0216c
srav001 <sravansuresh1x@gmail.com>
2022-09-15 03:41:52 +0530

    feat: process fb data

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,13 +3,35 @@ import { FB } from 'fb';
 
 @Injectable()
 export class AppService {
+	/**
+	 * It makes a Facebook API call, maps the data, and returns it in a format that is easier to use
+	 * @returns An object with three properties: campaignNames, campaignImpressions, and data.
+	 */
 	async getFBdata() {
+		/* This is a Facebook API call. */
 		FB.options({ version: 'v14.0' });
 		FB.setAccessToken(process.env.ACCESS_TOKEN);
 		const FBdata = await FB.api(
 			'act_25064918/insights?fields=campaign_id,campaign_name,impressions&level=campaign&time_range[since]=2021-01-01&time_range[until]=2022-01-01',
 			'get'
 		);
-		return FBdata;
+		/* Mapping the data from the Facebook API call and returning it in a format that is easier to use. */
+		const { data } = FBdata;
+		const campaignNames = [];
+		const campaignImpresssions = [];
+		const mappedData = data.map(obj => {
+			campaignNames.push(obj.campaign_name);
+			campaignImpresssions.push(obj.impressions);
+			return {
+				compaignName: obj.campaign_name,
+				impressions: obj.impressions
+			};
+		});
+
+		return {
+			campaignNames,
+			campaignImpresssions,
+			data: mappedData
+		};
 	}
 }
