ab0360c8a8d783cf4ca2fccb4783c9ff16daa870
srav001 <sravansuresh1x@gmail.com>
2022-09-15 03:19:17 +0530

    fix: routing

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,27 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import IndexView from '../views/Index.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+	mode: 'history',
+	base: import.meta.env.BASE_URL,
+	routes: [
+		{
+			path: '/',
+			name: 'index',
+			component: IndexView
+		}
+		// {
+		//   path: '/fb',
+		//   name: 'fb',
+		//   component: () => import('../views/fbView.vue')
+		// }
+	]
+});
 
 export default router
