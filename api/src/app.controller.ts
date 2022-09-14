a9d7fcbe193957d25433e90a0959aa619aed09f3
srav001 <sravansuresh1x@gmail.com>
2022-09-15 03:30:11 +0530

    feat: fetch fb data

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getFBdata(): string {
+  async getFBdata() {
     return this.appService.getFBdata();
   }
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { FB } from 'fb';
 
 @Injectable()
 export class AppService {
-	getFBdata() {
-		return 'FB DATA HERE!';
+	async getFBdata() {
+		FB.options({ version: 'v14.0' });
+		FB.setAccessToken(process.env.ACCESS_TOKEN);
+		const FBdata = await FB.api(
+			'act_25064918/insights?fields=campaign_id,campaign_name,impressions&level=campaign&time_range[since]=2021-01-01&time_range[until]=2022-01-01',
+			'get'
+		);
+		return FBdata;
 	}
 }
