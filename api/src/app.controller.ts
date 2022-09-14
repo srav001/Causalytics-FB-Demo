6fdc2cce1fb55ebfd2d96018097841f41efb8648
srav001 <sravansuresh1x@gmail.com>
2022-09-15 03:23:33 +0530

    feat: enable env files in api

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getFBdata(): string {
+    return this.appService.getFBdata();
   }
 }
